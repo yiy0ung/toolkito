96ecea32d517d2771d20bf6db64e6d44d1536d17
wlsdud2194 <wlsdud2194@gmail.com>
2021-09-12 02:40:32 +0900

    Fix: not found type file

--- src/utility/array.ts
+++ src/utility/array.ts
@@ -5,15 +5,12 @@
 //================================================================
 
 /**
- * 
- * @param arr 
- * @param selector 
- * @returns 
+ *
+ * @param arr
+ * @param selector
+ * @returns
  */
-export function normalize<T>(
-  arr: T[], 
-  selector: (item: T) => string | number = (item: any) => item.id
-) {
+export function normalize<T>(arr: T[], selector: (item: T) => string | number = (item: any) => item.id) {
   const normalizeObj: { [key: string]: T } = {};
 
   arr.forEach((data) => {
@@ -24,12 +21,33 @@ export function normalize<T>(
 }
 
 /**
+ *
+ * @param arr
+ * @param selector
+ * @returns
+ */
+export function groupBy<T>(arr: T[], selector: (item: T) => string | number = (item: any) => item.id) {
+  const map: Record<string, T[]> = {};
+
+  arr.forEach((data) => {
+    const key = selector(data);
+    if (map[key]) {
+      map[key].push(data);
+    } else {
+      map[key] = [data];
+    }
+  });
+
+  return map;
+}
+
+/**
  * @param array
  * @param callback
  */
- export async function asyncForEach<T>(
-  array: T[], 
-  callback: (value: T, idx: number) => Promise<void> | void,
+export async function asyncForEach<T>(
+  array: T[],
+  callback: (value: T, idx: number) => Promise<void> | void
 ): Promise<void> {
   for (let i = 0; i < array.length; i++) {
     await callback(array[i], i);
@@ -37,14 +55,11 @@ export function normalize<T>(
 }
 
 /**
- * @param array 
- * @param callback 
- * @returns 
+ * @param array
+ * @param callback
+ * @returns
  */
-export async function asyncMap<T, R>(
-  array: T[], 
-  callback: (value: T, idx: number) => Promise<R> | R,
-) {
+export async function asyncMap<T, R>(array: T[], callback: (value: T, idx: number) => Promise<R> | R) {
   const resultArr: R[] = new Array(array.length || 0);
 
   for (let i = 0; i < resultArr.length; i++) {
@@ -55,16 +70,13 @@ export async function asyncMap<T, R>(
 }
 
 /**
- * 
- * @param array 
- * @param callback 
- * @returns 
+ *
+ * @param array
+ * @param callback
+ * @returns
  */
-export async function asyncFilter<T>(
-  array: T[],
-  callback: (value: T, idx: number) => Promise<boolean> | boolean
-) {
-  let c = 0
+export async function asyncFilter<T>(array: T[], callback: (value: T, idx: number) => Promise<boolean> | boolean) {
+  let c = 0;
   const resultArr: T[] = [];
 
   for (let i = 0; i < array.length; i++) {
