ade8a52def5b3afff1310273ad679b758c5bdaec
wlsdud2194 <wlsdud2194@gmail.com>
2021-09-12 23:07:50 +0900

    Fix: support commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,23 @@
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
+const banner = `/* Toolkito: v${pkg.version} */`;
+
 export default {
   input: './src/index.ts',
   output: [
     {
       file: pkg.main,
-      format: 'esm',
-      banner: '/* eslint-disable */',
+      format: 'cjs',
       exports: 'auto',
+      banner,
     },
-    { file: pkg.common, format: 'cjs', exports: 'auto' },
-    { file: pkg.module, format: 'esm', exports: 'auto' },
+    {
+      file: pkg.module,
+      format: 'esm',
+      exports: 'auto',
+      banner,
+    },
   ],
   plugins: [typescript()],
 };
